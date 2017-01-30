0cc140888967b6a30f3d062356197c2ccc261a52
David Jalloh <codewithjalloh@gmail.com>
2017-01-30 23:25:22 +0000

    updated code

--- 01_imageViewProgrammatically/ViewController.swift
+++ 01_imageViewProgrammatically/ViewController.swift
@@ -12,14 +12,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let bear1 = UIImage(named: "1.jpg")!
+        let bear2 = UIImage(named: "2.jpg")!
+        let blackmoon = UIImage(named: "3.png")!
+        
+        
+        // imageview programatically
+        let myCoolImageView = UIImageView()
+        myCoolImageView.frame = CGRect(x: 10, y: 100, width: 400, height: 300)
+        myCoolImageView.image = bear1
+        
+        
+        
+        
+        myCoolImageView.highlightedImage = bear2
+        
+        myCoolImageView.isUserInteractionEnabled = true
+        myCoolImageView.isHighlighted = false
+        
+        myCoolImageView.animationImages = [bear1, bear2, blackmoon]
+        myCoolImageView.animationDuration = 0.6
+        myCoolImageView.animationRepeatCount = 0 // infinite
+        myCoolImageView.startAnimating()
+        
+        
+        self.view.addSubview(myCoolImageView)
+        
+        if myCoolImageView.isAnimating {
+            print("yes")
+        } else {
+            print("No")
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
